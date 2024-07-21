831c3b7eaecbaa76d7858b482f1764352a8ca1f8
sujiuzii <zengjianbin@mail.ustc.edu.cn>
2024-07-21 14:26:23 +0800

    Update todos and fix one bug

--- config/features.go
+++ config/features.go
@@ -1,15 +1,6 @@
 // Description: This file contains the features that can be used to
 package config
 
-// TODO: Add more features, and reconstruct the features to use
-// TODO: Browser fingerprint
-// 可信设备列表
-// features to use
-// var Features = map[string][]string{
-// 	"ip": {"ip", "isp", "city"},
-// 	"ua": {"browser", "os", "device"},
-// }
-
 var Features = map[string][]string{
 	"ip": {"ip", "isp", "region"},
 	"ua": {"browser", "os"},
